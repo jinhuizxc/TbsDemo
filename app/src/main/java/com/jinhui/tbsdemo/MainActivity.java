df869c692240bd5df5344d193c8a075e2cbdef08
jinhuizxc <1004260403@qq.com>
2018-09-06 00:10:31 +0800

    Initial commit

---
+++ app/src/main/java/com/jinhui/tbsdemo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.jinhui.tbsdemo;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mButton = findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] perms = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                if (!EasyPermissions.hasPermissions(MainActivity.this, perms)) {
+                    EasyPermissions.requestPermissions(MainActivity.this, "需要访问手机存储权限！", 10086, perms);
+                } else {
+                    startActivity(new Intent(MainActivity.this,TbsActivity.class));
+                }
+            }
+        });
+    }
+}
